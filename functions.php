7985eea7c65ebe949ce9b608a48b52e8d838ec19
Andrew Minion <andrew@andrewrminion.com>
2017-07-01 14:12:51 -0400

    add Google fonts

--- functions.php
+++ functions.php
@@ -1,1 +1,9 @@
 <?php
+
+/**
+ * Add Google Fonts
+ */
+function ic_google_fonts() {
+    wp_enqueue_style( 'google-fonts', 'https://fonts.googleapis.com/css?family=Niconne|Raleway:400,400i,700,700i' );
+}
+add_action( 'wp_enqueue_scripts', 'ic_google_fonts' );
